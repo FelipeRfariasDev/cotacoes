d6c346849db3fdb3ec308fd0e3d85802a7193c62
Felipe <felipe.farias.php@gmail.com>
2022-03-26 13:57:05 -0300

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         .container{
-            margin: auto;
             width: 100%;
         }
     </style>
@@ -16,7 +15,7 @@
   <body>
         <main class="py-4">
             <div class="container">
-                <h1><img src="/logo.png" width="70px"> Cotações</h1>
+                <h1>Cotações</h1>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
                         
