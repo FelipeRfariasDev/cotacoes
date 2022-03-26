864596ca92516293194e027ab46e1acd80f647ff
Felipe <felipe.farias.php@gmail.com>
2022-03-26 13:53:27 -0300

    ajuste formatação

--- resources/views/cotacoes/index.blade.php
+++ resources/views/cotacoes/index.blade.php
@@ -52,10 +52,20 @@
       <strong>Forma de pagamento</strong> {{$forma_pagamento}} <br>
       <strong>Valor da moeda de destino {{$nome_moeda_destino}}</strong> {{$valor_moeda_destino}}<br>
       <strong>Valor Total R$ </strong> {{$valor_total_reais}}<br><br>
-      Aplicar taxa de 2% pela conversão para valores abaixo de R$ 3.700,00 e 1% para valores maiores que R$ 3.700,01, essa taxa deve ser aplicada apenas no valor da compra e não sobre o valor já com a taxa de forma de pagamento R$ {{$vrl_taxa_conversao_reais}}<br><br>
-      Taxa de acordo com o Tipo de Pagamento Boleto taxa de 1,37% & Cartão de Crédito taxa de 7,73% foi aplicado a taxa de ({{$getJurosTipoPagamento[$forma_pagamento]}}%) totalizando R$ {{$getValorTipoPagamentoJuros}}<br><br>
-      Valor Total é R$ {{$valor_total_reais}} + Taxa Conversão ({{$getJurosValorTotalPercentual}}%) sendo o valor R$ {{$vrl_taxa_conversao_reais}} totalizando R$ {{$vrl_tot_mult_taxa_conversao}}<br><br>
-      Valor Total R$ {{$valor_total_reais}} + Taxa de acordo com o Tipo de Pagamento Boleto/Cartão ({{$getJurosTipoPagamento[$forma_pagamento]}}) sendo R$ {{$getValorTipoPagamentoJuros}} totalizando R$ {{$valor_total_reais_juros}} <br><br>
+
+      <strong>1 Regra -></strong> Aplicar taxa de 2% pela conversão para valores abaixo de R$ 3.700,00 e 1% para valores maiores que R$ 3.700,01, essa taxa deve ser aplicada apenas no valor da compra e não sobre o valor já com a taxa de forma de pagamento. <strong>Com base na especificação da regra o resultado é R$ {{$vrl_taxa_conversao_reais}}.</strong><br>
+      <strong>Taxa Conversão com base da <strong>Regra 1 </strong>({{$getJurosValorTotalPercentual}}%)</strong><br><br>
+
+      <strong>2 Regra -></strong>
+      Taxa de acordo com o Tipo de Pagamento Boleto taxa de 1,37% & Cartão de Crédito taxa de 7,73%. <strong>Com base na especificação da regra foi aplicado a taxa de ({{$getJurosTipoPagamento[$forma_pagamento]}}%) totalizando R$ {{$getValorTipoPagamentoJuros}}</strong><br><br>
+      
+      <strong>3 Regra -></strong>
+      Valor Total é R$ {{$valor_total_reais}} + Taxa Conversão ({{$getJurosValorTotalPercentual}}%) sendo o valor R$ {{$vrl_taxa_conversao_reais}}. <strong>Com base na especificação da regra o resultado é R$ {{$vrl_tot_mult_taxa_conversao}}</strong><br><br>
+      
+      <strong>4 Regra -></strong>
+      Valor Total R$ {{$valor_total_reais}} + Taxa de acordo com o Tipo de Pagamento Boleto/Cartão ({{$getJurosTipoPagamento[$forma_pagamento]}}) sendo R$ {{$getValorTipoPagamentoJuros}}.<strong>Com base na especificação da regra o resultado é R$ {{$valor_total_reais_juros}} </strong> <br><br>
+      
+      
       Cotação realizada em {{$data_hora_cotacao}}
   </div>
   @endif
