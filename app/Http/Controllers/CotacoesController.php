695afad9be2f40bdd1b95ca619fec7b29a61b7a0
Felipe <felipe.farias.php@gmail.com>
2022-03-26 11:34:48 -0300

    cotacoes

--- app/Http/Controllers/CotacoesController.php
+++ app/Http/Controllers/CotacoesController.php
@@ -13,28 +13,46 @@ class CotacoesController extends Controller
         $getValorMoedas = $cotacoes->getValorMoedas();
 
         if($_POST){
-            $valor_real_BRL = $_POST['valor_real_BRL'];
+            $valor_real_BRL     = $_POST['valor_real_BRL'];
+
+            if($valor_real_BRL<=1000 && $valor_real_BRL>=100000){
+                echo "O valor para conversão deve ser maior que R$ 1.000 e menor que R$ 100.000,00";
+                exit;
+            }
+
             $nome_moeda_destino = $_POST['nome_moeda_destino'];
-            $forma_pagamento = $_POST['forma_pagamento'];
+            $forma_pagamento    = $_POST['forma_pagamento'];
 
             $cotacoes = new Cotacoes();
-            $exibirValoresConvertido = $cotacoes->getValorConvertidoMoedaDestino($valor_real_BRL,$nome_moeda_destino);
-            $nome_moeda_destino = $exibirValoresConvertido["nome_moeda_destino"];
-            $valor_real_BRL = ($exibirValoresConvertido["valor_real_BRL"]);
-            $valor_moeda_destino = $exibirValoresConvertido["valor_moeda_destino"];
-            $valor_total_multiplicado = $exibirValoresConvertido["valor_total_multiplicado"];
-            $data_hora_cotacao = date('d/m/Y H:i:s', time());
+            $exibirValoresConvertido        = $cotacoes->getValorConvertidoMoedaDestino($valor_real_BRL,$nome_moeda_destino,$forma_pagamento);
+            $nome_moeda_destino             = $exibirValoresConvertido["nome_moeda_destino"];
+            $valor_real_BRL                 = ($exibirValoresConvertido["valor_real_BRL"]);
             
-            return view('cotacoes.index', [
-                "exibir_resultado"      =>  true,
-                "moeda_origem_blr"      =>  number_format($valor_real_BRL,2,',','.'),
-                "nome_moeda_destino"    =>  $nome_moeda_destino,
-                "valor_moeda_destino"   =>  $valor_moeda_destino,
-                "forma_pagamento"       =>  $forma_pagamento,
-                "valor_total_reais"     =>  number_format($valor_total_multiplicado,2,',','.'),
-                "data_hora_cotacao"     =>  $data_hora_cotacao,
-                "getValorMoedas"        =>  $getValorMoedas
+            $valor_moeda_destino            = $exibirValoresConvertido["valor_moeda_destino"];
+            $valor_total_multiplicado       = $exibirValoresConvertido["valor_total_multiplicado"];
+            $vrl_taxa_conversao_reais       = $exibirValoresConvertido["vrl_taxa_conversao_reais"];
+            
+            $vrl_tot_mult_taxa_conversao    = $exibirValoresConvertido["vrl_tot_mult_taxa_conversao"];
+            $valor_total_multiplicado_juros = $exibirValoresConvertido["valor_total_multiplicado_juros"];
+            $getJurosTipoPagamento          = $cotacoes->getJurosTipoPagamento();
+            $getJurosValorTotal             = $cotacoes->getJurosValorTotal();
+            $data_hora_cotacao              = date('d/m/Y H:i:s', time());
+            $getJurosValorTotalPercentual   = $cotacoes->getJurosValorTotalPercentual($valor_total_multiplicado);
 
+            return view('cotacoes.index', [
+                "exibir_resultado"              =>  true,
+                "moeda_origem_blr"              =>  number_format($valor_real_BRL,2,',','.'),
+                "nome_moeda_destino"            =>  $nome_moeda_destino,
+                "valor_moeda_destino"           =>  $valor_moeda_destino,
+                "forma_pagamento"               =>  $forma_pagamento,
+                "getJurosTipoPagamento"         =>  $getJurosTipoPagamento,
+                "vrl_taxa_conversao_reais"      =>  number_format($vrl_taxa_conversao_reais,2,',','.'),
+                "getJurosValorTotalPercentual"  =>  $getJurosValorTotalPercentual,
+                "valor_total_reais"             =>  number_format($valor_total_multiplicado,2,',','.'),
+                "vrl_tot_mult_taxa_conversao"   =>  number_format($vrl_tot_mult_taxa_conversao,2,',','.'),
+                "valor_total_reais_juros"       =>  number_format($valor_total_multiplicado_juros,2,',','.'),
+                "data_hora_cotacao"             =>  $data_hora_cotacao,
+                "getValorMoedas"                =>  $getValorMoedas
             ]);
 
         }
--- resources/views/cotacoes/index.blade.php
+++ resources/views/cotacoes/index.blade.php
@@ -6,7 +6,7 @@
 
   <div class="row">
     <div class="col-12">
-        <label class="col-form-label">Valor em Reais</label>
+        <label class="col-form-label">Valor(Compra) para conversão R$</label>
     </div>
   </div>
   <div class="row">
@@ -46,13 +46,31 @@
   @if($exibir_resultado)
   <div>
     
-      <h3>Cotações Resultado</h3>
+      <h3>Cotações Resultado Parâmetros de saída:</h3>
 
       <strong>Moeda de origem BRL R$</strong> {{$moeda_origem_blr}} <br>
-      <strong>Moeda de destino</strong> {{$nome_moeda_destino}} {{$valor_moeda_destino}}<br>
       <strong>Forma de pagamento</strong> {{$forma_pagamento}} <br>
-      <strong>R$ Valor Total</strong> {{$valor_total_reais}}<br>
+      <strong>Moeda de destino {{$nome_moeda_destino}}</strong> {{$valor_moeda_destino}}<br>
+      <strong>Valor Total R$ </strong> {{$valor_total_reais}}<br>
+      <strong>Taxa Conversão R$ </strong>{{$vrl_taxa_conversao_reais}}<br>
+      <strong>Valor Total + Taxa Conversão R$ ({{$getJurosValorTotalPercentual}}%) </strong> {{$vrl_tot_mult_taxa_conversao}}<br>
+      <strong>Valor Total + Juros R$ ({{$getJurosTipoPagamento[$forma_pagamento]}}) </strong> {{$valor_total_reais_juros}}<br>
       <strong>Cotação realizada em</strong> {{$data_hora_cotacao}}
+  </div>
+
+  <div>
+
+    <h3>Cotações Resultado Parâmetros de saída:</h3>
+
+    <strong>Moeda de origem:</strong> BRL <br>
+    <strong>Moeda de destino:</strong> USD <br>
+    <strong>Valor para conversão:</strong> R$ 5.000,00 <br>
+    <strong>Forma de pagamento:</strong> Boleto <br>
+    <strong>Valor da "Moeda de destino" usado para conversão:</strong> $ 5,30 <br>
+    <strong>Valor comprado em "Moeda de destino":</strong> $ 921,03 (taxas aplicadas no valor de compra diminuindo no valor total de conversão) <br>
+    <strong>Taxa de pagamento:</strong> R$ 68,50 <br>
+    <strong>Taxa de conversão:</strong> R$ 50,00 <br>
+    <strong>Valor utilizado para conversão descontando as taxas:</strong> R$ 4.881,50 
   </div>
   @endif
  
